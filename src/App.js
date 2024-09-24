52c7a4c29f1baa71c2608c8602a965ec72d84c11
Kiruthika Ramesh <lakshana192k4@gmail.com>
2024-09-24 12:24:35 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <h1>
+         name:charulakshana
+         place:vellore
+         mobno:9363685563
+         
+
+      </h1>
+    </div>
+  );
+}
+
+export default App;
